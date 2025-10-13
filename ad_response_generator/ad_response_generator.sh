27b38e524d68c5e6762eae9fb8329b9b6b7b0687
KrzysztofUrbanskiSam <k.urbanski@samsung.com>
2025-10-13 08:59:57 +0200

    Ad response improvements 3 (#23)
    * [Ad response generator] Changes in repository setuper
    
    * Fix after m.wiacek bug in producer wrapper

--- ad_response_generator/ad_response_generator.sh
+++ ad_response_generator/ad_response_generator.sh
@@ -9,20 +9,17 @@
 # Add localization parquet
 # localhost , port, DB kofigurowalne
 
-
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-printf "I ${RED}love${NC} Stack Overflow\n"
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-source ${SCRIPT_DIR}/utils/verification.sh
-source ${SCRIPT_DIR}/utils/argument_parser.sh
+REPO_DIR=$(git rev-parse --show-toplevel)
+# source ${REPO_DIR}/ad_response_generator/utils/print_helper.sh
+source ${REPO_DIR}/ad_response_generator/utils/verification.sh
+source ${REPO_DIR}/ad_response_generator/utils/argument_parser.sh
+source ${REPO_DIR}/ad_response_generator/utils/repository_setuper.sh
 
 DEBUG=false
 AD_LANGUAGE="en"
-BRANCH_BIDDER="main"
-BRANCH_DA="master"
+BRANCH_BIDDER=""
+BRANCH_DA=""
 REFRESH_DA_DATA=true
 
 DB_HOST="localhost"
@@ -30,7 +27,7 @@ DB_PORT=5432
 DB_USER="adgear"
 DB_NAME="rtb-trader-dev"
 
-OUTPUT="${SCRIPT_DIR}/runs/$(date '+%Y%m%d-%H%M%S')"
+OUTPUT="${REPO_DIR}/ad_response_generator/runs/$(date '+%Y-%m-%d/%H%M%S')"
 OUTPUT_JSON_CREATIVES=${OUTPUT}/creatives.parquet.tmp.json
 
 output_ad_requests=${OUTPUT}/ad_requests
@@ -38,23 +35,16 @@ output_ad_responses=${OUTPUT}/ad_responses
 output_artifacts=${OUTPUT}/artifacts
 output_logs=${OUTPUT}/logs
 output_setup=${OUTPUT}/setup
+output_backup=${OUTPUT}/backup
 
-ROOT_SQL_PREQA_CREATIVES=${ROOT_DATA_ACTIVATION}/sql/creatives/preqa_creatives.sql
-ROOT_SQL_TEST_TVS_CREATIVES=${ROOT_DATA_ACTIVATION}/sql/test_tvs_creatives/test_tvs_creatives.sql
-ROOT_SQL_CREATIVES_STRATEGY=${ROOT_DATA_ACTIVATION}/transformation/creatives/creativesStrategy.go
-_da_sql_preqa_creatives=${OUTPUT}/preqa_creatives.sql
-_da_sql_preqa_creatives_orig=${OUTPUT}/preqa_creatives.sql.orig
-_da_sql_ttc=${OUTPUT}/test_tvs_creatives.sql
-_da_sql_ttc_orig=${OUTPUT}/test_tvs_creatives.sql.orig
+ROOT_DATA_ACTIVATION=${ROOT_DATA_ACTIVATION}
+ROOT_BIDDER=${ROOT_BIDDER}
+
 
 ROOT_BIDDER_DOCKER_COMPOSE=${ROOT_BIDDER}/docker/bidder/docker-compose.deps.yml
 ROOT_BIDDER_CONFIG_LOCAL=${ROOT_BIDDER}/configs/bidder/default-local.yaml
 _bidder_docker_compose_orig=${OUTPUT}/docker-compose.deps.yml
 _bidder_config_local_orig=${OUTPUT}/default-local.yaml
-
-ROOT_GENERATED_DATA=${ROOT_DATA_ACTIVATION}/data-activation
-ROOT_GENERATED_TEST_TV_PARQUET=${ROOT_GENERATED_DATA}/test_tvs_creatives/parquet/test_tvs_creatives.parquet
-ROOT_GENERATED_PREQA_CREATIVES_PARQUET=${ROOT_GENERATED_DATA}/preqa_creatives/parquet/preqa_creatives.parquet
 
 PYTHON="/home/k.urbanski/.venv/bin/python"
 PYTHON_PARQUET_TO_JSON=${SCRIPT_DIR}/extract_parquet_files.py
@@ -63,35 +53,6 @@ PYTHON_PARQUET_TO_JSON=${SCRIPT_DIR}/extract_parquet_files.py
 CREATIVES_IDS=()
 CREATIVES_PIDS=()
 TVS_PSIDS=()
-
-function replace_where_clause(){
-    sql_file=$1
-    where_search=$2
-    where_clause=$3
-
-    if grep -q "$where_search" "${sql_file}"; then
-        sed -i "s/$where_search.*/$where_clause/" "${sql_file}"
-    else
-        sed -i -E "s/(.*)((ORDER|GROUP) BY 1)/\1$where_clause\n\1\2/" "${sql_file}"
-    fi
-}
-
-function setup_data_activation(){
-    # Modify preqa_creatives.sql to get affected creatives
-    local creative_ids_list=$(IFS=', '; echo "${CREATIVES_IDS[*]}")
-    local where_search="WHERE[[:space:]]\+vw_creatives"
-    local where_clause="WHERE vw_creatives.id IN ($creative_ids_list)"
-
-    cp ${ROOT_SQL_PREQA_CREATIVES} ${_da_sql_preqa_creatives_orig}
-    replace_where_clause "${ROOT_SQL_PREQA_CREATIVES}" "${where_search}" "${where_clause}"
-    cp ${ROOT_SQL_PREQA_CREATIVES} ${_da_sql_preqa_creatives}
-
-    local creative_ids_list=$(IFS=', '; echo "${CREATIVES_IDS[*]}")
-    local where_search="WHERE[[:space:]]\+creative_id"
-    local where_clause="WHERE creative_id IN ($creative_ids_list)"
-    cp ${ROOT_SQL_TEST_TVS_CREATIVES} ${_da_sql_ttc_orig}
-    replace_where_clause "${ROOT_SQL_TEST_TVS_CREATIVES}" "${where_search}" "${where_clause}"
-}
 
 function setup_bidder(){
     cp ${ROOT_BIDDER_DOCKER_COMPOSE} ${_bidder_docker_compose_orig}
@@ -208,7 +169,7 @@ function generate_test_tv_data(){
     echo "INFO: Generating Test TV data. May take 10s ..."
     rm -f ${ROOT_GENERATED_TEST_TV_PARQUET}
     cd ${ROOT_DATA_ACTIVATION}
-    ./dev-run.sh test_tvs_creatives &> /dev/null
+    ./dev-run.sh test_tvs_creatives &> ${OUTPUT}/logs/data-activation-test_tvs.txt
     if [ ! -e ${ROOT_GENERATED_TEST_TV_PARQUET} ]; then
         echo "Failed to generate test_tvs_creatives parquet. Exiting ..." && exit 1
     fi
@@ -218,7 +179,7 @@ function generate_preqa_creatives_data(){
     echo "INFO: Generating preqa creatives data. May take 15s"
     rm -f ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET}
     cd ${ROOT_DATA_ACTIVATION}
-    ./dev-run.sh preqa_creatives &> /dev/null
+    ./dev-run.sh preqa_creatives &> ${OUTPUT}/logs/data-activation-preqa-creatives.txt
     if [ ! -e ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ]; then
         echo "ERROR: Failed to generate preqa_creatives parquet. Exiting ..." && exit 1
     fi
@@ -241,6 +202,8 @@ function parse_parquet_files() {
 
 function populate_bidder_with_data() {
     echo "INFO: populating bidder with DA data ..."
+    echo "BIDDER data to check: ${ROOT_GENERATED_TEST_TV_PARQUET}"
+    echo $ROOT_GENERATED_TEST_TV_PARQUET $ROOT_GENERATED_PREQA_CREATIVES_PARQUET
     cp ${ROOT_GENERATED_TEST_TV_PARQUET} ${ROOT_BIDDER}/test/data-activation/data
     cp ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ${ROOT_BIDDER}/test/data-activation/data/preqa_creatives.parquet
     cp ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ${ROOT_BIDDER}/test/data-activation/data/creatives.parquet
@@ -249,8 +212,8 @@ function populate_bidder_with_data() {
 function run_bidder_services(){
     echo "INFO: starting bidder services ..."
     cd ${ROOT_BIDDER}
-    make stop-local-env &> /dev/null
-    make start-local-env &> ${OUTPUT}/logs/bidder_services.txt
+    make stop-local-env &> ${OUTPUT}/logs/bidder_services_stop.txt
+    make start-local-env &> ${OUTPUT}/logs/bidder_services_start.txt
 }
 
 function run_bidder(){
@@ -296,6 +259,9 @@ function handle_exit(){
 parse_arguments "$@"
 DB_CONNECT="psql -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -d ${DB_NAME}"
 do_verification
+setup_da_branch ${BRANCH_DA}
+setup_bidder_branch ${BRANCH_BIDDER}
+
 setup_test_tvs ${CREATIVES_IDS[@]}
 setup_data_activation
 setup_bidder
---
+++ ad_response_generator/utils/repository_setuper.sh
@@ -0,0 +1,174 @@
+function verify_file_exists() {
+    if [[ ! -f "$1" ]]; then
+        echo "ERROR: Needed file does not exists: $1"
+        exit 1
+    fi
+}
+
+function pull_default_branch() {
+    local repo_path="$1"
+    local default_branch="$2"
+    local current_branch="$3"
+    local repo_name=${1##*/}
+
+    if [[ "${current_branch}" == "${default_branch}" ]]; then
+        local pull_log="${output_logs}/pull_${repo_name}_${default_branch}.log"
+        $DEBUG && { echo "DEBUG: Pulling newest changes for ${repo_path}"; }
+        cd ${repo_path}
+        if ! command git pull origin "$default_branch" &> ${pull_log}; then
+            echo "WARNING: Failed to pull '${default_branch}' for '${repo_path}'"
+            echo "HINT: Inspect logs from ${pull_log}. Probably you have unstaged changes"
+        fi
+    fi
+}
+
+function checkout_on_branch() {
+    local repo_path="$1"
+    local target_branch="$2"
+    local current_branch
+    local default_branch
+
+    if [[ ! -d "$repo_path/.git" ]]; then
+        echo "Error: '$repo_path' is not a git repository"
+        exit 1
+    fi
+
+    cd $repo_path
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
+    default_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's|^refs/remotes/origin/||')
+
+    if [[ -z "${target_branch}" ]]; then
+        $DEBUG && { echo "DEBUG: Not changing branch, staying on '${current_branch}'"; }
+        return
+    fi
+
+    if [[ "$current_branch" == "$target_branch" ]]; then
+        $DEBUG && { echo "DEBUG: In '${repo_path}' already on branch '$target_branch'"; }
+        pull_default_branch $repo_path $default_branch $current_branch
+        return
+    fi
+
+   if git show-ref --verify --quiet refs/heads/"$target_branch"; then
+        # Branch exists locally
+        echo "DEBUG: Checking on local branch '$target_branch'... "
+        git checkout "$target_branch" &>/dev/null || {
+            echo "ERROR: Failed to checkout local branch '$target_branch'"
+            exit 1
+        }
+    elif git ls-remote --heads origin "$target_branch" | grep -q "$target_branch"; then
+        # Branch exists remotely
+        echo "DEBUG: Checking on remote branch '$target_branch'... "
+        git checkout "$target_branch" &>/dev/null || {
+            echo "Error: Failed to checkout remote branch '$target_branch'"
+            return 7
+        }
+    else
+        echo "ERROR: For '${repo_path}' branch '$target_branch' does not exist neither locally nor remotely ..."
+        exit 1
+    fi
+
+    pull_default_branch $repo_path $default_branch $current_branch
+}
+
+function locate_repository(){ # $1 = repository name
+    local REPO_DIR="/home/k.urbanski/Projects/Education"
+    readarray -t REPOS < <(find ${REPO_DIR}/../ -name .git -type d -printf "%h\n")
+    for repo in ${REPOS[@]}; do
+        if command cat "${repo}/.git/config" | grep "$1.git" &>/dev/null ; then
+            echo $(readlink -f "${repo}")
+            exit 0
+        fi
+    done
+}
+
+function setup_git_repository() {
+    local repo_name="$1"
+    local repo_branch="$2"
+    local repo_url="$3"
+    local root_env_var="$4"
+
+    # Get the value of the root environment variable
+    local root_path="${!root_env_var}"
+
+    if [[ -z "${root_path}" ]]; then
+        echo "WARNING: Env variable '${root_env_var}' not set. Trying to guess ${repo_name}"
+        echo "HINT: Set '${root_env_var}' pointing to root of ${repo_name} repository to avoid search"
+        root_path=$(locate_repository "${repo_name}")
+    fi
+
+    if ! command cat $root_path/.git/config 2>/dev/null | grep "${repo_name}.git" &>/dev/null ; then
+        echo "ERROR: Set '${root_env_var}' pointing to root of ${repo_name} repository"
+        echo "INFO: Please pull repo from: ${repo_url}"
+        exit 1
+    fi
+
+    export ${root_env_var}=${repo_path}
+    eval "export ${root_env_var}=\"${root_path}\""
+    checkout_on_branch ${root_path} ${repo_branch}
+}
+
+function setup_bidder_branch() {
+    local repo_branch="$1"
+
+    setup_git_repository "rtb-bidder" "${repo_branch}" "https://github.com/adgear/rtb-bidder" "ROOT_BIDDER"
+}
+
+function setup_da_branch() {
+    local repo_branch="$1"
+
+    setup_git_repository "data-activation-producer-wrapper" "${repo_branch}" "https://github.com/adgear/data-activation-producer-wrapper" "ROOT_DATA_ACTIVATION"
+
+    ROOT_SQL_PREQA_CREATIVES=${ROOT_DATA_ACTIVATION}/sql/creatives/preqa_creatives.sql
+    ROOT_SQL_TEST_TVS_CREATIVES=${ROOT_DATA_ACTIVATION}/sql/test_tvs_creatives/test_tvs_creatives.sql
+    ROOT_SQL_CREATIVES_STRATEGY=${ROOT_DATA_ACTIVATION}/transformation/creatives/creativesStrategy.go
+
+    # Rename to backup/setup
+    _da_sql_preqa_creatives=${output_backup}/preqa_creatives.sql
+    _da_sql_preqa_creatives_orig=${output_backup}/preqa_creatives.sql.orig
+    _da_sql_ttc=${output_backup}/test_tvs_creatives.sql
+    _da_sql_ttc_orig=${output_backup}/test_tvs_creatives.sql.orig
+
+    verify_file_exists ${ROOT_SQL_PREQA_CREATIVES}
+    verify_file_exists ${ROOT_SQL_TEST_TVS_CREATIVES}
+    verify_file_exists ${ROOT_SQL_CREATIVES_STRATEGY}
+}
+
+
+function setup_data_activation(){
+    # Modify preqa_creatives.sql to get affected creatives
+    local creative_ids_list=$(IFS=', '; echo "${CREATIVES_IDS[*]}")
+    local sql_creative_limitation_for_preqa_creatives="vw_creatives.id IN ($creative_ids_list)"
+    local sql_creative_limitation_for_tvs="creative_id IN ($creative_ids_list)"
+
+    cp ${ROOT_SQL_PREQA_CREATIVES} ${_da_sql_preqa_creatives_orig}
+    cp ${ROOT_SQL_TEST_TVS_CREATIVES} ${_da_sql_ttc_orig}
+
+    # Replace in preqa_creatives.sql
+    sed -i -r -e "/WHERE\s+.*/d" "${ROOT_SQL_PREQA_CREATIVES}"
+    sed -i -r -e "s|(ORDER BY.*)|WHERE ${sql_creative_limitation_for_preqa_creatives}\n\1|" "${ROOT_SQL_PREQA_CREATIVES}"
+
+    # replace in test_tvs_creatives.sql
+    sed -i -r -e "s|WHERE\s+(test_tvs.*)|WHERE ${sql_creative_limitation_for_tvs} AND \1|" "${ROOT_SQL_TEST_TVS_CREATIVES}"
+    sed -i -r -e "s|WHERE\s+creative_id.*AND\s+(.*)|WHERE ${sql_creative_limitation_for_tvs} AND \1|" "${ROOT_SQL_TEST_TVS_CREATIVES}"
+
+    # HOPE: this is just temporary substitiution
+    sed -i -r -e "s|sql/test_devices_creatives/test_devices_creatives.sql|sql/test_tvs_creatives/test_tvs_creatives.sql|" ${ROOT_DATA_ACTIVATION}/transformation/test_tvs_creatives.go
+
+    cp ${ROOT_SQL_PREQA_CREATIVES} ${_da_sql_preqa_creatives}
+    cp ${ROOT_SQL_TEST_TVS_CREATIVES} ${_da_sql_ttc}
+
+    # Verify sed correctness
+    if ! command grep -q "${sql_creative_limitation_for_preqa_creatives}" "${ROOT_SQL_PREQA_CREATIVES}" ; then
+        echo "ERROR: Failed to modify ${ROOT_SQL_PREQA_CREATIVES} to limit to focused creatives"
+        exit 1
+    fi
+
+    if ! command grep -q "${sql_creative_limitation_for_tvs}" "${ROOT_SQL_TEST_TVS_CREATIVES}" ; then
+        echo "ERROR: Failed to modify ${ROOT_SQL_TEST_TVS_CREATIVES} to limit to focused creatives"
+        exit 1
+    fi
+
+    ROOT_GENERATED_DATA=${ROOT_DATA_ACTIVATION}/data-activation
+    ROOT_GENERATED_TEST_TV_PARQUET=${ROOT_GENERATED_DATA}/test_tvs_creatives/parquet/test_tvs_creatives.parquet
+    ROOT_GENERATED_PREQA_CREATIVES_PARQUET=${ROOT_GENERATED_DATA}/preqa_creatives/parquet/preqa_creatives.parquet
+}
