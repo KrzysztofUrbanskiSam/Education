00e9c82d15e5cfe557079ebd856b3127bffa7486
k.urbanski <k.urbanski@samsung.com>
2025-11-24 14:29:54 +0100

    Split large main file into smaller more specialized

---
+++ ad_response_generator/utils/handle_bidder.sh
@@ -0,0 +1,99 @@
+function setup_bidder_parquet() {
+    cd ${ROOT_BIDDER}
+
+    echo "INFO: Populating bidder with 'make parquet' command"
+    make parquet &> ${output_logs}/bidder_make_parquet.log
+    if [ ! -e "${ROOT_BIDDER}/test/data-activation/data/flights.parquet" ]; then
+        echo "ERROR: Populating bidder with 'make parquet' command"
+        echo "INFO: Inspect log ${output_logs}/bidder_make_parquet.log"
+        exit 1
+    fi
+}
+
+function setup_bidder_geoip(){
+    local correct_geoip=true
+    geoip_files=("/usr/share/GeoIP/DE-CountryISO-DB.mmdb" "/usr/share/GeoIP/GeoIP2-Connection-Type.mmdb" "/usr/share/GeoIP/GeoIP2-ISP.mmdb")
+    for f in ${geoip_files[@]}; do
+        if [ ! -e "$f" ]; then
+            echo "ERROR: Missing GeoIP file: $f"
+            correct_geoip=false
+        fi
+    done
+
+    #TODO: Consider replacing usage of /usr/share/GeoIP so not 'sudo' directory would be needed
+    #      Right now script is not adjsuted (and probably will never be) to run with sudo permissions
+    if [ "$correct_geoip" = false ]; then
+        echo "HINT: sudo mkdir -p /usr/share/GeoIP/ && sudo cp -f ${ROOT_BIDDER}/test/data-activation/data/GeoIP/* /usr/share/GeoIP/"
+        exit 1
+    fi
+}
+
+function setup_bidder(){
+    cd ${ROOT_BIDDER}
+
+    setup_bidder_parquet
+    setup_bidder_geoip
+
+    cp ${ROOT_BIDDER_DOCKER_COMPOSE} ${_bidder_docker_compose_orig}
+    cp ${ROOT_BIDDER_CONFIG_LOCAL} ${_bidder_config_local_orig}
+    cp ${ROOT_BIDDER_APP_GO} ${_bidder_app_go_orig}
+
+    sed -i -r -e "s|^\s+(-.*fake-barker)|# \1|" ${ROOT_BIDDER_DOCKER_COMPOSE}
+    sed -i -r -e "s|^\s+(-.*userprofileservice)|# \1|" ${ROOT_BIDDER_DOCKER_COMPOSE}
+    sed -i -r -e "s|^\s+(-.*crossdeviceprofile)|# \1|" ${ROOT_BIDDER_DOCKER_COMPOSE}
+    sed -i -r -e "s|^\s+(-.*fake-ups)|# \1|" ${ROOT_BIDDER_DOCKER_COMPOSE}
+    sed -i -r -e "s|(\s*err\s*:=\s*)unleashclient.InitializeUnleashClient.*|\1error(nil)|" "${ROOT_BIDDER_APP_GO}"
+    sed -i '/^event_publisher:$/ { n; s/true/false/ }' ${ROOT_BIDDER_CONFIG_LOCAL}
+    sed -i -r -e 's|(\s*)loopinterval: 10s|\1loopinterval: 10000000s|' ${ROOT_BIDDER_CONFIG_LOCAL}
+    sed -i -r -e "s|(\s+host:\s*\")localhost:8085\"|\1rtb-canary.adgrx.com\"|" ${ROOT_BIDDER_CONFIG_LOCAL}
+    sed -i '/^familyhub:$/ { n; s/true/false/ }' ${ROOT_BIDDER_CONFIG_LOCAL}
+
+    # No longer needed but keep it here
+    # sed -i -r -e "s|url:\s*.*unleash.*|url: http://localhost:51000|g" ${ROOT_BIDDER_CONFIG_LOCAL}
+    # sed -i -r -e "s|(^run-rtb-bidder:).*(#.*)|\1 \2|" ${ROOT_BIDDER}/Makefile
+    # sed -i -r -e "/docker compose.*docker-compose.yml logs/d" ${ROOT_BIDDER}/Makefile
+}
+
+function setup_bidder_branch() {
+    local repo_branch="$1"
+
+    setup_git_repository "rtb-bidder" "${repo_branch}" "https://github.com/adgear/rtb-bidder" "ROOT_BIDDER"
+
+    ROOT_BIDDER_DOCKER_COMPOSE=${ROOT_BIDDER}/docker/bidder/docker-compose.deps.yml
+    ROOT_BIDDER_CONFIG_LOCAL=${ROOT_BIDDER}/configs/bidder/default-local.yaml
+    ROOT_BIDDER_APP_GO=${ROOT_BIDDER}/internal/bidder/app/app.go
+    verify_file_exists ${ROOT_BIDDER_DOCKER_COMPOSE}
+    verify_file_exists ${ROOT_BIDDER_CONFIG_LOCAL}
+    verify_file_exists ${ROOT_BIDDER_APP_GO}
+
+    _bidder_docker_compose_orig=${OUTPUT}/docker-compose.deps.yml
+    _bidder_config_local_orig=${OUTPUT}/default-local.yaml
+    _bidder_app_go_orig=${OUTPUT}/app.go
+}
+
+function populate_bidder_with_data() {
+    echo "INFO: Populating bidder with DA data ..."
+    if [ ! -e ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ]; then
+        echo "ERROR: Generated 'preqa_creatives' data not found."
+        if [[ $REFRESH_DA_DATA == false ]]; then
+            echo "HINT: Rerun script without '--no-da-refresh' flag"
+        fi
+        exit 1
+    fi
+    cp ${ROOT_GENERATED_TEST_TV_PARQUET} ${ROOT_BIDDER}/test/data-activation/data
+    cp ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ${ROOT_BIDDER}/test/data-activation/data/preqa_creatives.parquet
+    cp ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ${ROOT_BIDDER}/test/data-activation/data/creatives.parquet
+    if [ ${TV_LANGUAGE} != "en" ]; then
+        if [ ! -e ${ROOT_GENERATED_LOCALIZATION_PARQUET} ]; then
+            cp ${ROOT_GENERATED_LOCALIZATION_PARQUET} ${ROOT_BIDDER}/test/data-activation/data/localization.parquet
+        else
+            echo "WARNING: Localization parquet file not found. Default 'Ad language' will be 'en'"
+        fi
+    fi
+}
+
+function run_bidder(){
+    echo "INFO: Starting bidder ..."
+    cd ${ROOT_BIDDER}
+    go run ${ROOT_BIDDER}/cmd/bidder/ -configFile ${ROOT_BIDDER_CONFIG_LOCAL} &> ${OUTPUT}/logs/bidder.txt
+}
---
+++ ad_response_generator/utils/handle_data_activation.sh
@@ -0,0 +1,185 @@
+function setup_da_branch() {
+    local repo_branch="$1"
+
+    setup_git_repository "data-activation-producer-wrapper" "${repo_branch}" "https://github.com/adgear/data-activation-producer-wrapper" "ROOT_DATA_ACTIVATION"
+
+    ROOT_DEV_RUN="${ROOT_DATA_ACTIVATION}/dev-run.sh"
+    ROOT_SQL_PREQA_CREATIVES=${ROOT_DATA_ACTIVATION}/sql/creatives/preqa_creatives.sql
+    ROOT_SQL_TEST_TVS_CREATIVES=${ROOT_DATA_ACTIVATION}/sql/test_tvs_creatives/test_tvs_creatives.sql
+    ROOT_SQL_CREATIVES_STRATEGY=${ROOT_DATA_ACTIVATION}/transformation/creatives/creativesStrategy.go
+    ROOT_TEST_TVS_CREATIVES="${ROOT_DATA_ACTIVATION}/transformation/test_tvs_creatives.go"
+    ROOT_METRIX_INFLUXDB="${ROOT_DATA_ACTIVATION}/pkg/common_packages/metric/influxdb.go"
+    verify_file_exists ${ROOT_SQL_PREQA_CREATIVES}
+    verify_file_exists ${ROOT_SQL_TEST_TVS_CREATIVES}
+    verify_file_exists ${ROOT_SQL_CREATIVES_STRATEGY}
+    verify_file_exists ${ROOT_DEV_RUN}
+    verify_file_exists ${ROOT_TEST_TVS_CREATIVES}
+    verify_file_exists ${ROOT_METRIX_INFLUXDB}
+
+    # Rename to backup/setup
+    _da_dev_run=${output_backup}/dev-run.sh
+    _da_sql_preqa_creatives=${output_backup}/preqa_creatives.sql
+    _da_sql_preqa_creatives_orig=${output_backup}/preqa_creatives.sql.orig
+    _da_sql_ttc=${output_backup}/test_tvs_creatives.sql
+    _da_sql_ttc_orig=${output_backup}/test_tvs_creatives.sql.orig
+    _da_sql_creatives_strategy_orig=${output_backup}/creativesStrategy.go.orig
+    _da_test_tvs_creatives=${output_backup}/test_tvs_creatives.go
+    _da_metrix_influx_db=${output_backup}/influxdb.go
+}
+
+
+function setup_data_activation() {
+    # Modify preqa_creatives.sql to get affected creatives
+    local creative_ids_list=$(IFS=', '; echo "${CREATIVES_IDS[*]}")
+    local sql_creative_limitation_for_preqa_creatives="vw_creatives.id IN ($creative_ids_list)"
+    local sql_creative_limitation_for_tvs="creative_id IN ($creative_ids_list)"
+
+    cp ${ROOT_DEV_RUN} ${_da_dev_run}
+    cp ${ROOT_SQL_PREQA_CREATIVES} ${_da_sql_preqa_creatives_orig}
+    cp ${ROOT_SQL_TEST_TVS_CREATIVES} ${_da_sql_ttc_orig}
+    cp ${ROOT_SQL_CREATIVES_STRATEGY} ${_da_sql_creatives_strategy_orig}
+    cp ${ROOT_TEST_TVS_CREATIVES} ${_da_test_tvs_creatives}
+    cp ${ROOT_METRIX_INFLUXDB} ${_da_metrix_influx_db}
+
+    # Replace in dev-run.sh to avoid S3 uploads
+    sed -i -r -e "s|(^\s+)(aws s3 cp.*)|\1\# \2|g" ${ROOT_DEV_RUN}
+
+    # Replace influxdb to avoid metrics sending
+    sed -i -r -e "s/^(\s*func.*(Incr|Count).*\{).*/\1 return/g" ${ROOT_METRIX_INFLUXDB}
+    if ! command grep -qe "func.*Incr.*return" "${ROOT_METRIX_INFLUXDB}" ; then
+        echo "WARNING: Failed to modify 'Incr' in ${ROOT_METRIX_INFLUXDB} to not send metrics"
+    fi
+    if ! command grep -qe "func.*Count.*return" "${ROOT_METRIX_INFLUXDB}" ; then
+        echo "WARNING: Failed to modify 'Count' in ${ROOT_METRIX_INFLUXDB} to not send metrics"
+    fi
+
+    # Replace in preqa_creatives.sql
+    sed -i -r -e "/WHERE\s+.*/d" "${ROOT_SQL_PREQA_CREATIVES}"
+    sed -i -r -e "s|(ORDER BY.*)|WHERE ${sql_creative_limitation_for_preqa_creatives}\n\1|" "${ROOT_SQL_PREQA_CREATIVES}"
+
+    # replace in test_tvs_creatives.sql
+    if command grep -q -E "\s*WHERE" ${ROOT_SQL_TEST_TVS_CREATIVES}; then
+        sed -i -r -e "s|WHERE\s*(test_tvs.*)|WHERE ${sql_creative_limitation_for_tvs} AND \1|" "${ROOT_SQL_TEST_TVS_CREATIVES}"
+        sed -i -r -e "s|WHERE\s*creative_id.*AND\s+(.*)|WHERE ${sql_creative_limitation_for_tvs} AND \1|" "${ROOT_SQL_TEST_TVS_CREATIVES}"
+        sed -i -r -e "s|(\s*)WHERE creative_id IN .*\)|\1WHERE ${sql_creative_limitation_for_tvs}|" "${ROOT_SQL_TEST_TVS_CREATIVES}"
+    else
+        sed -i -r -e "s|(\s*)(GROUP.*)|\1WHERE ${sql_creative_limitation_for_tvs}\n\1\2|" "${ROOT_SQL_TEST_TVS_CREATIVES}"
+    fi
+
+    # Verify sed correctness
+    if ! command grep -q "${sql_creative_limitation_for_preqa_creatives}" "${ROOT_SQL_PREQA_CREATIVES}" ; then
+        echo "ERROR: Failed to modify ${ROOT_SQL_PREQA_CREATIVES} to limit to focused creatives"
+        exit 1
+    fi
+
+    # HOPE: this is just temporary substitiution
+    # TODO: Add limitation to test_devices_creatives
+    # sed -i -r -e "s|sql/test_devices_creatives/test_devices_creatives.sql|sql/test_tvs_creatives/test_tvs_creatives.sql|" ${ROOT_TEST_TVS_CREATIVES}
+    # if ! command grep -q "test_tvs_creatives.sql" "${ROOT_TEST_TVS_CREATIVES}" ; then
+    #     echo "ERROR: Failed to modify ${ROOT_TEST_TVS_CREATIVES} to focus on creatives"
+    #     exit 1
+    # fi
+
+    # Modify creativesStrategy.go to disable S3 upload
+    sed -i -r -e "s|(\s+)(WriteToS3.*filepath)|\1//\2|" ${ROOT_SQL_CREATIVES_STRATEGY}
+
+    cp ${ROOT_SQL_PREQA_CREATIVES} ${_da_sql_preqa_creatives}
+    cp ${ROOT_SQL_TEST_TVS_CREATIVES} ${_da_sql_ttc}
+
+    if ! command grep -q "${sql_creative_limitation_for_tvs}" "${ROOT_SQL_TEST_TVS_CREATIVES}" ; then
+        echo "ERROR: Failed to modify ${ROOT_SQL_TEST_TVS_CREATIVES} to limit to focused creatives"
+        exit 1
+    fi
+
+    ROOT_GENERATED_DATA=${ROOT_DATA_ACTIVATION}/data-activation
+    ROOT_GENERATED_DATA_PREQUA_CREATIVES_TERM=${ROOT_GENERATED_DATA}/preqa_creatives/term
+    ROOT_GENERATED_TEST_TV_PARQUET=${ROOT_GENERATED_DATA}/test_tvs_creatives/parquet/test_tvs_creatives.parquet
+    ROOT_GENERATED_PREQA_CREATIVES_PARQUET=${ROOT_GENERATED_DATA}/preqa_creatives/parquet/preqa_creatives.parquet
+    ROOT_GENERATED_LOCALIZATION_PARQUET=${ROOT_GENERATED_DATA}/localization/parquet/localization.parquet
+}
+
+function generate_test_tv_data(){
+    echo "INFO: Generating Test TV data..."
+    rm -f ${ROOT_GENERATED_TEST_TV_PARQUET}
+    cd ${ROOT_DATA_ACTIVATION}
+    ${ROOT_DEV_RUN} test_tvs_creatives &> ${OUTPUT}/logs/data-activation-test_tvs.txt
+    if [ ! -e ${ROOT_GENERATED_TEST_TV_PARQUET} ]; then
+        echo "Failed to generate test_tvs_creatives parquet. Exiting ..." && exit 1
+    fi
+    echo "INFO: Generated parquet for test_tvs: ${ROOT_GENERATED_TEST_TV_PARQUET}"
+}
+
+function generate_preqa_creatives_data(){
+    echo "INFO: Generating preqa creatives data..."
+    rm -f ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET}
+    cd ${ROOT_DATA_ACTIVATION}
+    ${ROOT_DEV_RUN} preqa_creatives &> ${OUTPUT}/logs/data-activation-preqa-creatives.txt
+    if [ ! -e ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ]; then
+        echo "ERROR: Failed to generate preqa_creatives parquet. Exiting ..." && exit 1
+    fi
+}
+
+function generate_localization_data(){
+    echo "INFO: Generating localization data..."
+    cd ${ROOT_DATA_ACTIVATION}
+    rm -f ${ROOT_GENERATED_LOCALIZATION_PARQUET}
+    ${ROOT_DEV_RUN} localization &> ${OUTPUT}/logs/data-activation-localization.txt
+    if [ ! -e ${ROOT_GENERATED_LOCALIZATION_PARQUET} ]; then
+        echo "ERROR: Failed to generate localization parquet. Exiting ..." && exit 1
+    fi
+}
+
+function convert_da_parquet_to_json() {
+    ${PYTHON} ${PYTHON_PARQUET_TO_JSON} ${ROOT_GENERATED_PREQA_CREATIVES_PARQUET} ${OUTPUT_JSON_CREATIVES}
+    ${PYTHON} ${PYTHON_PARQUET_TO_JSON} ${ROOT_GENERATED_TEST_TV_PARQUET} ${OUTPUT_TEST_TVS}
+    echo "INFO: Output in json for preqa_creatives: ${OUTPUT_JSON_CREATIVES}"
+    echo "INFO: Output in json for test_tvs_creatives: ${OUTPUT_TEST_TVS}"
+}
+
+function process_term_bert_files() {
+    echo "INFO: Processing term bert files ..."
+    term_files=$(find ${ROOT_GENERATED_DATA_PREQUA_CREATIVES_TERM}/ -type f -name "*.term" | xargs)
+
+    for creative_id in ${CREATIVES_IDS[@]}; do
+        local creative_term_file="${output_artifacts}/${creative_id}_term.term"
+        local creative_bert_file="${output_artifacts}/${creative_id}_bert.bert2"
+        touch ${creative_term_file} ${creative_bert_file}
+        term_file_found=false
+        for term_file in $term_files; do
+            if grep -q "$creative_id" "${term_file}"; then
+                term_file_found=true
+                bert_file=$(dirname ${term_file})/creatives.bert2
+                cp ${term_file} ${creative_term_file}
+                # echo "INFO: For ${creative_id} generated term file: ${creative_term_file}"
+                CREATIVES_TERM+=("${creative_term_file}")
+
+                if [[ -e ${bert_file} ]]; then
+                    cp ${bert_file} ${creative_bert_file}
+                    CREATIVES_BERT+=("${creative_bert_file}")
+                    # echo "INFO: For ${creative_id} generated bert file: ${creative_bert_file}"
+                else
+                    echo "WARNING: For ${creative_id} cannot find bert file: ${creative_bert_file}"
+                    CREATIVES_BERT+=("${creative_bert_file}${EMPTY_MARK}")
+                fi
+                break
+            fi
+        done
+        if [ "$term_file_found" = false ]; then
+            echo "ERROR: No term and bert files found for creative_id $creative_id"
+            CREATIVES_BERT+=("${creative_bert_file}${EMPTY_MARK}")
+            CREATIVES_TERM+=("${creative_term_file}${EMPTY_MARK}")
+        fi
+    done
+}
+
+function parse_parquet_files() {
+    while IFS= read -r line; do
+        id=$(echo "$line" | jq -r '.Id')
+        creative_parquet_out_json=${output_artifacts}/${id}_da_json.json
+        echo $line | jq --indent 2 . &> ${creative_parquet_out_json}
+        # echo "INFO: For creative_id: $id parquet file: ${creative_parquet_out_json}"
+        CREATIVES_PARQUETS+=("${creative_parquet_out_json}")
+
+    done < ${OUTPUT_JSON_CREATIVES}
+
+}
